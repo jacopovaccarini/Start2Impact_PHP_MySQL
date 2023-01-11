8aa5462f90790b91d925b43bb8b7ac47fead9f97
jacopovaccarini <52632159+jacopovaccarini@users.noreply.github.com>
2023-01-11 11:43:18 +0100

    Update commit

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ $database = new Database();
 $db = $database->getConnection();
 $queryBuilder = new QueryBuilder($db);
 
-$tasks = $queryBuilder->selectAll('prestazioni_offerte');
+$offerte = $queryBuilder->selectAll('prestazioni_offerte');
+$erogate = $queryBuilder->selectAll('prestazioni_erogate');
+$sommaOfferte = $queryBuilder->sumAll('prestazioni_offerte','Tempo');
+$sommaErogate = $queryBuilder->sumAll('prestazioni_erogate','Quantita');
+$unioni = $queryBuilder->joinAll('prestazioni_offerte','prestazioni_erogate');
 
 require 'index.view.php';
--- models/QueryBuilder.php
+++ models/QueryBuilder.php
@@ -11,10 +11,28 @@ class QueryBuilder
 
   public function selectAll($table)
   {
-    $statement = $this->pdo->prepare("select * from {$table}");
+    $stmt = $this->pdo->prepare("select * from {$table}");
 
-    $statement->execute();
+    $stmt->execute();
 
-    return $statement->fetchAll(PDO::FETCH_CLASS);
+    return $stmt->fetchAll(PDO::FETCH_CLASS);
+  }
+
+  public function sumAll($table, $column)
+  {
+    $stmt = $this->pdo->prepare("select sum({$column}) from {$table}");
+
+    $stmt->execute();
+
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public function joinAll($table, $join)
+  {
+    $stmt = $this->pdo->prepare("select * from {$table} inner join {$join} on {$table}.Nome = {$join}.Tipologia");
+
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_CLASS);
   }
 }
