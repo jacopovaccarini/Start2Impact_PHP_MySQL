53dc6528001057ddcd0a8c8798cbe0680f9bc4d9
jacopovaccarini <52632159+jacopovaccarini@users.noreply.github.com>
2023-03-06 14:43:24 +0100

    Update commit

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 return [
   'database' => [
-    'name' => 'Bonny',
-    'username' => 'root',
-    'password' => '',
-    'connection' => 'mysql:host=127.0.0.1',
+    'name' => $_ENV['DATABASE'],
+    'username' => $_ENV['USERNAME'],
+    'password' => $_ENV['PASSWORD'],
+    'connection' => $_ENV['CONNECTION'],
     'options' => [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]
